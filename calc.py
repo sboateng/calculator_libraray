9fa02fc8075c8778fc8ba28b988d84dc2a24bd89
root <root@LAPTOP-GKQSNA4V.localdomain>
2020-05-13 23:07:35 -0400

    add function for add and sub

---
+++ calc.py
@@ -0,0 +1,10 @@
+"""Calculator Libaraay basic math Operation"""
+
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
